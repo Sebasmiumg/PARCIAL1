77a6d5fce27e7ad5fc417d1d2e5801548d7c6e5e
Sebasmiumg <jrossac@miumg.edu.gt>
2024-03-02 10:27:59 -0600

    ejercicio2.2 de jose rossa 0909-23-8514

---
+++ EJERCICIO2.2.cpp
@@ -0,0 +1,13 @@
+//USANDO LA ESTRUCTURA DE CONTROL "FOR" HAS UN CICLO INICIANDO DE CERO, QUE INCREMENTE DE 3 EN 3 HASTA LLEGAR AL PRIMER NUMERO IMPAR DESPUES DE 30.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numero = 0;
+
+    for (numero = 0; numero <= 30; numero += 3) {
+    }
+    cout << "El primer numero impar despues de 30 es: " << numero << endl;
+
+    return 0;
+}
